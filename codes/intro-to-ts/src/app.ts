d33729b8091e67650255ac4be3a3a3822c555693
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2025-04-30 14:51:37 +0600

    type differences

--- codes/intro-to-ts/src/app.ts
+++ codes/intro-to-ts/src/app.ts
@@ -15,3 +15,28 @@
 // function f(s) {           // error: parameter s implicitly has an any type
 //     console.log(typeof s); // string
 // }
+
+// interface Point {
+//     readonly x: number;
+//     readonly y: number;
+//   }
+  
+//   const p1: Point = { x: 10, y: 20 };
+//   p1.x++ // ❌ Error: Cannot assign to 'x' because it is a read-only property.
+
+// function a() : undefined {
+//     return null;
+// }
+
+let a : any;
+
+function test (a: any) {
+    console.log(a.toFixed(2)); // 0.00
+}
+
+let b: unknown;
+
+function test2 (b: unknown) {
+
+    typeof(b) === 'number' ? console.log(b.toFixed(2)) : '';
+}
